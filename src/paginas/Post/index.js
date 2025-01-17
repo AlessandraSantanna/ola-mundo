108f938e0f4a1dbc9de6f61c64300278cd5f12ce
AlessandraSantanna <alesy2007@gmail.com>
2025-01-17 03:23:14 -0300

    alinhando textos

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -9,43 +9,52 @@ import styles from './Post.module.css';
 import PostCard from '../../componentes/PostCard';
 
 export default function Post() {
-  const { id } = useParams(); // Desestruturação para obter o parâmetro 'id'
+  const parametros = useParams();
 
-  const post = posts.find((post) => post.id === Number(id));
+  const post = posts.find((post) => {
+      return post.id === Number(parametros.id);
+  })
 
   if (!post) {
     return <NaoEncontrado />;
   }
 
   const postsRecomendados = posts
-    .filter((post) => post.id !== Number(id))
-    .sort((a, b) => b.id - a.id)
-    .slice(0, 4);
+  .filter((post) => post.id !== Number(parametros.id))
+  .sort((a, b) => b.id - a.id)
+  .slice(0, 4);
+
 
   return (
     <Routes>
-      <Route path="*" element={<PaginaPadrao />}>
-        <Route index element={
-            <PostModelo
-              fotoCapa={`/assets/posts/${post.id}/capa.png`}
-              titulo={post.titulo}
-            >
-              <div className="post-markdown-container">
-                <ReactMarkdown>{post.texto}</ReactMarkdown>
-              </div>
-              <h2 className={styles.tituloOutrosPosts}>
-                Outros Posts que você pode gostar:
-              </h2>
-              <ul className={styles.postsRecomendados}>
-                {postsRecomendados.map((post) => (
-                  <li key={post.id}>
-                    <PostCard post={post} />
-                  </li>
-                ))}
-              </ul>
-            </PostModelo>
-        }/>
-      </Route>
+        <Route path="*" element={<PaginaPadrao />}>
+            <Route index element={
+                <PostModelo
+                    fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                    titulo={post.titulo}
+                >
+                    <div className="post-markdown-container">
+                        <ReactMarkdown>
+                            {post.texto}
+                        </ReactMarkdown>
+                    </div>
+
+                <h2 className={styles.tituloOutrosPosts}>
+                    Outros posts que você pode gostar:
+                </h2>
+
+                <ul className={styles.postsRecomendados}>
+                    {postsRecomendados.map((post) => (
+                        <li key={post.id}>
+                            <PostCard post={post} />
+                        </li>
+                    ))}
+                </ul>
+
+                </PostModelo>
+            } />
+        </Route>
     </Routes>
-  );
+
+)
 }
